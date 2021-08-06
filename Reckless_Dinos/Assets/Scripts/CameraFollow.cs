25c593bf9c8c024eff634e4c8aa055edb49793dc
MohammedAlawna <mohamedalawnae@yahoo.com>
2021-08-06 19:00:28 +0300

    1

---
+++ Reckless_Dinos/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+
+    [Range(1, 10)]
+    [SerializeField] float sFactor;
+
+    private void FixedUpdate()
+    {
+        Follow();
+
+    }
+
+    private void Follow()
+    {
+        Vector3 targetPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, targetPos
+            , sFactor * Time.fixedDeltaTime);
+        transform.position = smoothedPos;
+    }
+}
