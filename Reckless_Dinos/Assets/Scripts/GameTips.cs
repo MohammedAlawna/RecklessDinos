041d92b271525863bfc203301723266f854e67e3
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2021-08-18 12:07:39 +0300

    _buggyTips

--- Reckless_Dinos/Assets/Scripts/GameTips.cs
+++ Reckless_Dinos/Assets/Scripts/GameTips.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class GameTips : MonoBehaviour
 {
     [SerializeField] bool _showGameTips = false;
+    [SerializeField] bool _showCoinTips = false;
 
     public GameObject _tipsBox;
     public GameObject _tipsTools;
+    public GameObject _coinTipsBox;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +20,34 @@ public class GameTips : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_showGameTips)
+        if(_showGameTips && !_showCoinTips)
         {
             // _tipsBox.SetActive(false);
             _tipsBox.gameObject.GetComponent<Renderer>().enabled = false;
 
             _tipsTools.SetActive(true);
+            _coinTipsBox.SetActive(false);
         }
-        else
+        if(_showCoinTips && !_showGameTips)
+        {
+            _tipsBox.gameObject.GetComponent<Renderer>().enabled = false;
+            _tipsTools.SetActive(false);
+            _coinTipsBox.SetActive(true);
+        }
+       /* else
         {
             // _tipsBox.SetActive(true);
             _tipsBox.gameObject.GetComponent<Renderer>().enabled = true;
             _tipsTools.SetActive(false);
+            _coinTipsBox.SetActive(false);
 
+        }*/
+       else if(_showCoinTips && _showGameTips == false)
+        {
+            // _tipsBox.SetActive(true);
+            _tipsBox.gameObject.GetComponent<Renderer>().enabled = true;
+            _tipsTools.SetActive(false);
+            _coinTipsBox.SetActive(false);
         }
         
     }
@@ -38,16 +55,33 @@ public class GameTips : MonoBehaviour
  
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        _showCoinTips = false;
+        _showGameTips = false;
+
+       
+        /*
         if (collision.gameObject.tag == "Player")
         {
             _showGameTips = true;
-        }
+        }*/
       
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {      
-     _showGameTips = false;   
+        if(gameObject.name == "TipsBox")
+        {
+            _showGameTips = false;
+        }
+       
+
+        if (gameObject.name == "CoinTipsBox")
+        {
+            _showCoinTips = false;
+        } 
+       
+       
     }
 
 
