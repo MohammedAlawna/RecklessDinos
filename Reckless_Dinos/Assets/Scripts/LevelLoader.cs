54546693eeb98a04c89cbd226e0eabdb6582c306
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2021-10-05 23:18:31 +0300

    _addedSplashScreen

--- Reckless_Dinos/Assets/Scripts/LevelLoader.cs
+++ Reckless_Dinos/Assets/Scripts/LevelLoader.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float timeToLoad = 4f;
+    private IEnumerator Start()
     {
-        
+        yield return new WaitForSeconds(timeToLoad);
+        SceneManager.LoadScene("MainMenu");
     }
 
     // Update is called once per frame
