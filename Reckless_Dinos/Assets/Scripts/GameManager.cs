40efd293e1a10c5aee2d594f4a2d7debc75c16a1
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2021-11-14 10:37:54 -0800

    Update GameManager.cs

--- Reckless_Dinos/Assets/Scripts/GameManager.cs
+++ Reckless_Dinos/Assets/Scripts/GameManager.cs
@@ -21,7 +21,8 @@ public class GameManager : MonoBehaviour
     public int _noKnives = 0; 
     public bool _gamePaused = false;
     public bool _gameOver = false;
-    
+
+     
 
     //Process Singleton Pattern. 
     public static GameManager _singletonVar;
